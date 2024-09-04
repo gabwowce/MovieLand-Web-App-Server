3961f38257a4d25e7e49f15ae5366ad644b57fce
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-09-04 07:35:03 +0300

    Update User.js

--- auth/User.js
+++ auth/User.js
@@ -1,1 +1,38 @@
+//npm install sequelize mysql2 bcrypt
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('./db');
 
+const User = sequelize.define('User',{
+  id:{
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncreament: true
+  },
+  username:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate:{
+      isEmail: true
+    }
+  },
+  password_hash{
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  created_at:{
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+},{
+  timestamps: false,
+  tableName: 'users'
+});
+
+module.exports = User;
