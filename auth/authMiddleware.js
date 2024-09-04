3cf220f09c27b3c8774bb3d603ed13839026b1d5
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-09-04 08:06:01 +0300

    Update authMiddleware.js

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,1 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
+
+    if (!token) return res.status(401).json({ message: 'Token is missing' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
 
