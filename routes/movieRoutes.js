4b914c923e58d22baa950db894618c557401beb8
Gabrielė <134537965+gabwowce@users.noreply.github.com>
2024-09-05 08:32:00 +0300

    Create movieRoutes.js

---
+++ routes/movieRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+// to get all movies
+router.get('/', async (req,res)=>{
+  try{
+    const movies = await Movie.findAll();
+    res.json(movies);
+  }catch(error){
+    res.status(500).json({error: 'Failed to fetch movies'});
+  }
+})
+
+// to get movies by year/category
+router.get('/movies/filter' async(req,res)=>{
+  const {year, category} = req.query;
+
+  const whereClause = {};
+  if(year) whereClause.release_year = year;
+  if(category) whereClause.category = category
+  try{
+    const movies = await Movie.findAll({where: whereClause});
+    res.json(movies);
+  }catch(error){
+    res.status(500).json({error: 'Failed to fetch movies by filter'});
+  }
+})
+
+module.exports = router;
